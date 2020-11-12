a2b94b47b55a7f82ac4ea2914f58535311cc60ce
Matt Neuber <matt.neuber@avb.net>
2020-11-12 12:24:35 -0600

    Make query parameters optional

--- src/api/dto/pagination-query.dto.ts
+++ src/api/dto/pagination-query.dto.ts
@@ -1,12 +1,14 @@
 import { Transform } from 'class-transformer';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class PaginationQueryDto {
 	@Transform((x) => parseInt(x))
 	@IsNumber()
+	@IsOptional()
 	page?: number;
 
 	@Transform((x) => parseInt(x))
 	@IsNumber()
+	@IsOptional()
 	itemsPerPage?: number;
 }
